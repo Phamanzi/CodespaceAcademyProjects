126746ee6df6df3f5b0a51d1d1a60806f70ac250
Phamanzi <79787846+Phamanzi@users.noreply.github.com>
2021-10-10 18:13:32 +0200

    Interactive Web Apps First Challenge

---
+++ Script/rainbow.js
@@ -0,0 +1,33 @@
+function Red(){
+	document.getElementById('block1').style.backgroundColor = 'Red' 
+
+
+    document.getElementById("block1").style.color = "red";
+
+}
+function Orange(){
+	document.getElementById('block2').style.backgroundColor = 'Orange' 
+document.getElementById("block1").style.color = "red";
+} function Green(){
+	document.getElementById('block4').style.backgroundColor = 'green' 
+
+} function Blue(){
+	document.getElementById('block5').style.backgroundColor = 'Blue' 
+
+}
+    function Yellow(){
+	document.getElementById('block3').style.backgroundColor = 'Yellow' 
+
+}
+
+function Purple(){
+	document.getElementById('block6').style.backgroundColor = 'Purple' 
+
+}
+
+function LightPurple(){
+	document.getElementById('block7').style.backgroundColor = 'violet' 
+
+}
+
+
